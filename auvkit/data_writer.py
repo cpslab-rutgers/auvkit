0631d617e788b9f2e256587675ae0c2baf5adec2
mnadev <mohammednadeem902@gmail.com>
2020-07-17 12:24:38 -0400

    removed header argument from the CSV writer

--- auvkit/data_writer.py
+++ auvkit/data_writer.py
@@ -2,19 +2,18 @@ import csv
 from datetime import datetime
 
 
-def _csv_write(data_fetcher, csv_header, filename):
+def _csv_write(data_fetcher, filename):
     """
     This function is a private function to write data to a CSV until
     termination.
     :param data_fetcher: A DataFetcher object which describes which data to write to the CSV.
-    :param csv_header: The CSV header as a list.
     :param filename: The filename to write the CSV to.
     :return: None
     """
     file = open(filename, 'w')
     
     writer = csv.writer(file)
-    writer.writerow(csv_header)
+    writer.writerow(data_fetcher.get_header())
 
     while True:
         line = list()
@@ -25,26 +24,24 @@ def _csv_write(data_fetcher, csv_header, filename):
         writer.writerow(line)
 
 
-def create_csv(data_fetcher, csv_header, filename):
+def create_csv(data_fetcher, filename):
     """
     This function creates a CSV and then starts writing to the CSV until termination.
     :param data_fetcher: A DataFetcher object which describes which data to write to the CSV.
-    :param csv_header: The CSV header as a list.
     :param filename: The filename to write the CSV to.
     :return: None
     """
     if filename is None:
         filename = "data_" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.csv'
 
-    _csv_write(data_fetcher, csv_header, filename)
+    _csv_write(data_fetcher, filename)
 
 
-def csv_write(data_fetcher, csv_header, filename=None):
+def csv_write(data_fetcher, filename=None):
     """
     This is the publicly called function to write data to a CSV.
     :param data_fetcher: A DataFetcher object which describes which data to write to the CSV.
-    :param csv_header: The CSV header as a list.
     :param filename: The filename to write the CSV to.
     :return: None
     """
-    create_csv(data_fetcher, csv_header, filename)
+    create_csv(data_fetcher, filename)
