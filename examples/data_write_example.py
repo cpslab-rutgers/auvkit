0a52962d71837248f132cce8fbfbaf9548c80918
mnadev <mohammednadeem902@gmail.com>
2020-07-17 12:29:14 -0400

    added example showing how to write to csv

---
+++ examples/data_write_example.py
@@ -0,0 +1,13 @@
+from auvkit.path import Path, PathNode
+
+# Create a Path Object
+path = Path()
+
+# Call function to write to CSV, can specify filename if needed.
+path.write_to_csv()
+
+# Move 5 meters at an angle change of 45 degrees.
+# Then move, 10 meters at an angle change of 90 degrees.
+path.push(PathNode(5, 45, 0))
+path.push(PathNode(10, 90, 0))
+path.move_entire_route()
